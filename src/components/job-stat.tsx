5124a69d666d940a86bc862abcdf2d68f5cf22c5
harshmangalam <harshdev8218@gmail.com>
2023-07-05 16:54:31 +0530

    added numbers stats

--- src/components/job-stat.tsx
+++ src/components/job-stat.tsx
@@ -1,10 +1,12 @@
-import { component$ } from "@builder.io/qwik";
+import { Slot, component$ } from "@builder.io/qwik";
 
-export const JobStats = component$(
-  ({ icon, title, count }: { icon: any; title: string; count: number }) => {
+export const JobStat = component$(
+  ({ title, count }: { title: string; count: number }) => {
     return (
       <div class="stat">
-        <div class="stat-figure text-primary">{icon}</div>
+        <div class="stat-figure text-primary">
+          <Slot name="icon" />
+        </div>
         <div class="stat-title">{title}</div>
         <div class="stat-value text-primary">{count}</div>
       </div>
