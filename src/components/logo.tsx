137fc2d5b9a11238ab2e277894fdc5709c2d7610
harshmangalam <harshdev8218@gmail.com>
2023-07-05 15:12:57 +0530

    updated logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,8 +3,8 @@ import { Link } from "@builder.io/qwik-city";
 
 export const Logo = component$(() => {
   return (
-    <Link href="/" class="btn btn-ghost normal-case text-xl">
-      <h1 class="text-2xl font-semibold">QWIKJOBS</h1>
+    <Link href="/" class="text-2xl font-semibold">
+      <h1>QWIKJOBS</h1>
     </Link>
   );
 });
