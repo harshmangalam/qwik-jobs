4ac1c58c84f7cfea61f3a1eec3ba537b968f40d0
harshmangalam <harshdev8218@gmail.com>
2023-07-05 21:46:45 +0530

    added experience level

--- src/components/filters/experience-level.tsx
+++ src/components/filters/experience-level.tsx
@@ -3,23 +3,19 @@ import { component$ } from "@builder.io/qwik";
 export const ExperienceLevel = component$(() => {
   return (
     <div class="form-control">
-      <label class="label cursor-pointer">
-        <span class="label-text">Internship</span>
-        <input type="checkbox" class="checkbox" />
+      <label for="jobType" class="label cursor-pointer">
+        <span class="label-text">Experience Level</span>
       </label>
-
-      <label class="label cursor-pointer">
-        <span class="label-text">Entry level</span>
-        <input type="checkbox" class="checkbox" />
-      </label>
-      <label class="label cursor-pointer">
-        <span class="label-text">Associate</span>
-        <input type="checkbox" class="checkbox" />
-      </label>
-      <label class="label cursor-pointer">
-        <span class="label-text">Senior level</span>
-        <input type="checkbox" class="checkbox" />
-      </label>
+      <select
+        name="jobType"
+        id="jobType"
+        class="select select-bordered select-primary"
+      >
+        <option value="Internship">Internship</option>
+        <option value="Junior">Entry level</option>
+        <option value="Associate">Associate</option>
+        <option value="Senior">Senior level</option>
+      </select>
     </div>
   );
 });
--- src/routes/jobs/index.tsx
+++ src/routes/jobs/index.tsx
@@ -1,10 +1,10 @@
 import { component$ } from "@builder.io/qwik";
 import { Form } from "@builder.io/qwik-city";
+import { ExperienceLevel } from "~/components/filters/experience-level";
 import { JobType } from "~/components/filters/job-type";
 import { JobCard } from "~/components/job-card";
 
 export default component$(() => {
-  const;
   return (
     <div class="py-8 container mx-auto">
       <h1 class="text-4xl font-display font-bold"> All Qwik.js jobs </h1>
@@ -22,15 +22,7 @@ export default component$(() => {
             <article class="card bg-base-200">
               <div class="card-body p-4">
                 <div class="card-title">Filters</div>
-                <div>
-                  <p class="">Job type</p>
-                  <JobType />
-                </div>
-                <div class="divider" />
-                <div>
-                  <p class="">Experience Level</p>
-                </div>
-
+                <ExperienceLevel />
                 <div class="card-actions">
                   <button type="submit" class="btn btn-primary btn-block">
                     Apply
