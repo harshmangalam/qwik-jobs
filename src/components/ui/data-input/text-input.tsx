815b1473a1f5527e45f277f828fc28b0ce8c2fb3
harshmangalam <harshdev8218@gmail.com>
2023-07-06 17:05:06 +0530

    added text-input component

---
+++ src/components/ui/data-input/text-input.tsx
@@ -0,0 +1,24 @@
+import { type QwikIntrinsicElements, component$ } from "@builder.io/qwik";
+
+type InputProps = QwikIntrinsicElements["input"];
+type CustomProps = {
+  label: string;
+  error?: string | undefined;
+};
+type TextInputProps = InputProps & CustomProps;
+export const TextInput = component$((props: TextInputProps) => {
+  const { label, id, error, ...inputProps } = props;
+  return (
+    <div class="form-control">
+      <label for={id} class="label">
+        <span class="label-text">{label}</span>
+      </label>
+      <input class="input input-bordered" id={id} {...inputProps} />
+      {error && (
+        <label class="label">
+          <span class="label-text-alt text-error">Bottom Left label</span>
+        </label>
+      )}
+    </div>
+  );
+});
--- src/routes/post-a-job/index.tsx
+++ src/routes/post-a-job/index.tsx
@@ -1,16 +1,38 @@
 import { component$ } from "@builder.io/qwik";
 import { Form } from "@builder.io/qwik-city";
+import { TextInput } from "~/components/ui/data-input/text-input";
 
 export default component$(() => {
   return (
-    <div class="py-10 max-w-3xl mx-auto">
+    <div class="py-10 max-w-3xl w-full mx-auto">
       <div class="font-semibold text-2xl font-display">
         Let's begin with your company information
       </div>
       <div class="text-lg text-gray-600 mt-4">
         Let us know about your organization.
       </div>
-      <Form class="mt-8"></Form>
+      <Form class="mt-8">
+        <TextInput
+          label="Name"
+          placeholder="ACME Info..."
+          id="name"
+          name="name"
+        />
+        <div class="grid grid-cols-2 gap-4">
+          <TextInput
+            label="Website"
+            placeholder="https://website.com"
+            id="website"
+            name="website"
+          />
+          <TextInput
+            label="Twitter"
+            placeholder="@twitter"
+            id="twitter"
+            name="twitter"
+          />
+        </div>
+      </Form>
     </div>
   );
 });
