acac586de78d5802e24cf02e79b7d22ca12c16e5
harshmangalam <harshdev8218@gmail.com>
2023-07-10 21:31:03 +0530

    added user account details

--- src/routes/account/user/index.tsx
+++ src/routes/account/user/index.tsx
@@ -1,10 +1,25 @@
 import { component$ } from "@builder.io/qwik";
 import { Form } from "@builder.io/qwik-city";
+import { Button } from "~/components/ui/actions/button";
+import { TextInput } from "~/components/ui/data-input/text-input";
 
 export default component$(() => {
   return (
     <div>
-      <Form></Form>
+      <Form>
+        <TextInput label="Name" id="name" name="name" />
+        <TextInput label="Email" id="email" name="email" />
+        <div class="mt-6">
+          <Button type="submit" colorScheme="btn-primary">
+            Save changes
+            <iconify-icon
+              width={24}
+              height={24}
+              icon="formkit:arrowright"
+            ></iconify-icon>
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 });
