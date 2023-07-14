70aa545ba364436db9dc96d51828e5907099f879
harshmangalam <harshdev8218@gmail.com>
2023-07-14 12:33:07 +0530

    updated adapter

--- src/adapters/drizzle.ts
+++ src/adapters/drizzle.ts
@@ -1,5 +1,49 @@
 import type { Adapter } from "@auth/core/adapters";
+import { type PostgresJsDatabase } from "drizzle-orm/postgres-js";
 
-export function DrizzleAdapter(config?: any): Adapter {
-  return {};
+export function DrizzleAdapter(db: PostgresJsDatabase): Adapter {
+  return {
+    async createUser(user) {
+      return;
+    },
+    async getUser(id) {
+      return;
+    },
+    async getUserByEmail(email) {
+      return;
+    },
+    async getUserByAccount({ providerAccountId, provider }) {
+      return;
+    },
+    async updateUser(user) {
+      return;
+    },
+    async deleteUser(userId) {
+      return;
+    },
+    async linkAccount(account) {
+      return;
+    },
+    async unlinkAccount({ providerAccountId, provider }) {
+      return;
+    },
+    async createSession({ sessionToken, userId, expires }) {
+      return;
+    },
+    async getSessionAndUser(sessionToken) {
+      return;
+    },
+    async updateSession({ sessionToken }) {
+      return;
+    },
+    async deleteSession(sessionToken) {
+      return;
+    },
+    async createVerificationToken({ identifier, expires, token }) {
+      return;
+    },
+    async useVerificationToken({ identifier, token }) {
+      return;
+    },
+  };
 }
