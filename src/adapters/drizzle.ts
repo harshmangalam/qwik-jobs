2cd86a1d30f80c92b42df5126a99e939647fbe8c
harshmangalam <harshdev8218@gmail.com>
2023-07-14 15:39:00 +0530

    updated drizzle adapter

--- src/adapters/drizzle.ts
+++ src/adapters/drizzle.ts
@@ -1,25 +1,51 @@
-import type { Adapter } from "@auth/core/adapters";
+import type { Adapter, AdapterUser } from "@auth/core/adapters";
+import { eq } from "drizzle-orm";
 import { type PostgresJsDatabase } from "drizzle-orm/postgres-js";
-
-export function DrizzleAdapter(db: PostgresJsDatabase): Adapter {
+import { accounts } from "~/db/schema/accounts";
+import { type NewUser, users } from "~/db/schema/users";
+import * as schema from "~/db/schema";
+export function DrizzleAdapter(db: PostgresJsDatabase<typeof schema>): Adapter {
   return {
-    async createUser(user) {
-      return;
+    async createUser(user: Omit<AdapterUser, "id">) {
+      const data = await db
+        .insert(users)
+        .values(user as NewUser)
+        .returning();
+      return data[0] as AdapterUser;
     },
     async getUser(id) {
-      return;
+      const data = await db.select().from(users).where(eq(users.id, id));
+      return data[0] as AdapterUser;
     },
     async getUserByEmail(email) {
-      return;
+      const data = await db.select().from(users).where(eq(users.email, email));
+      return data[0] as AdapterUser;
     },
-    async getUserByAccount({ providerAccountId, provider }) {
-      return;
+    async getUserByAccount({ providerAccountId }) {
+      const data = await db.query.accounts.findFirst({
+        where: eq(accounts.providerAccountId, providerAccountId),
+        with: {
+          users: true,
+        },
+      });
+
+      return data?.users ? (data.users as AdapterUser) : null;
     },
-    async updateUser(user) {
-      return;
+    async updateUser({ id, ...data }) {
+      const user = await db
+        .update(users)
+        .set(data)
+        .where(eq(users.id as any, id))
+        .returning();
+
+      return user[0] as AdapterUser;
     },
     async deleteUser(userId) {
-      return;
+      const data = await db
+        .delete(users)
+        .where(eq(users.id, userId))
+        .returning();
+      return data[0] as AdapterUser;
     },
     async linkAccount(account) {
       return;
