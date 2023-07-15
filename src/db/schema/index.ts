c20568bf52607add81604e35f2e5853389728305
harshmangalam <harshdev8218@gmail.com>
2023-07-15 19:21:30 +0530

    drop drizzle support

--- src/db/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./accounts";
-export * from "./companies";
-export * from "./jobs";
-export * from "./sessions";
-export * from "./users";
-export * from "./verificationToken";
--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,14 +1,11 @@
 import { serverAuth$ } from "@builder.io/qwik-auth";
 import GitHub from "@auth/core/providers/github";
 import type { Provider } from "@auth/core/providers";
-import { DrizzleAdapter } from "~/adapters/drizzle";
-import { db } from "~/db";
 
 export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
   serverAuth$(({ env }) => ({
     secret: env.get("AUTH_SECRET"),
     trustHost: true,
-    adapter: DrizzleAdapter(db),
     providers: [
       GitHub({
         clientId: env.get("GITHUB_ID")!,
