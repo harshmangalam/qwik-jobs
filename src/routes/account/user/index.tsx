bc61680e9bd0bad15d9a6a9d3909b0fd82717b17
harshmangalam <harshdev8218@gmail.com>
2023-07-16 11:43:44 +0530

    updated user info

--- src/routes/account/user/index.tsx
+++ src/routes/account/user/index.tsx
@@ -1,16 +1,74 @@
+import { type Session } from "@auth/core/types";
 import { component$ } from "@builder.io/qwik";
-import { Form } from "@builder.io/qwik-city";
+import {
+  Form,
+  routeAction$,
+  routeLoader$,
+  z,
+  zod$,
+} from "@builder.io/qwik-city";
 import { Button } from "~/components/ui/actions/button";
 import { TextInput } from "~/components/ui/data-input/text-input";
+import { prisma } from "~/lib/prisma";
 
+export const useUser = routeLoader$(async ({ sharedMap, redirect }) => {
+  const session: Session | null = sharedMap.get("session");
+  if (!session?.user?.email) {
+    throw redirect(303, "/login");
+  }
+  return prisma.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
+});
+export const useUpdateUser = routeAction$(
+  async ({ email, name }, { redirect, sharedMap }) => {
+    const session: Session | null = sharedMap.get("session");
+    if (!session?.user?.email) {
+      return redirect(303, "/login");
+    }
+    await prisma.user.update({
+      where: {
+        email: session.user.email,
+      },
+      data: {
+        email,
+        name,
+      },
+    });
+
+    redirect(303, "/account/user/");
+  },
+  zod$({
+    name: z.string(),
+    email: z.string(),
+  })
+);
 export default component$(() => {
+  const user = useUser();
+  const action = useUpdateUser();
   return (
     <div>
-      <Form>
-        <TextInput label="Name" id="name" name="name" />
-        <TextInput label="Email" id="email" name="email" />
+      <Form action={action}>
+        <TextInput
+          value={user.value?.name ?? ""}
+          label="Name"
+          id="name"
+          name="name"
+        />
+        <TextInput
+          label="Email"
+          id="email"
+          name="email"
+          value={user.value?.email ?? ""}
+        />
         <div class="mt-6">
-          <Button type="submit" colorScheme="btn-primary">
+          <Button
+            loading={action.isRunning}
+            type="submit"
+            colorScheme="btn-primary"
+          >
             Save changes
             <iconify-icon
               width={24}
