2b918e03b60926062053c7a57920cc88fea63320
harshmangalam <harshdev8218@gmail.com>
2023-07-16 08:37:48 +0530

    added prisma adapter

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,11 +1,14 @@
 import { serverAuth$ } from "@builder.io/qwik-auth";
 import GitHub from "@auth/core/providers/github";
 import type { Provider } from "@auth/core/providers";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "~/lib/prisma";
 
 export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
   serverAuth$(({ env }) => ({
     secret: env.get("AUTH_SECRET"),
     trustHost: true,
+    adapter: PrismaAdapter(prisma),
     providers: [
       GitHub({
         clientId: env.get("GITHUB_ID")!,
