a75fe92be71aa99f7ebff272a73dd36cfea508db
harshmangalam <harshdev8218@gmail.com>
2023-07-19 17:51:19 +0530

    added base price card

---
+++ src/routes/post-a-job/base-price-card.tsx
@@ -0,0 +1,64 @@
+import { component$ } from "@builder.io/qwik";
+
+export const BasePriceCard = component$(() => {
+  const postingConditions = [
+    {
+      title: "Active for 30 days",
+      subtitle: "Your opening will be live for 30 days.",
+    },
+    {
+      title: "Included in our weekly newsletter",
+      subtitle:
+        "Get your job delivered to our entire database of active job seekers.",
+    },
+
+    {
+      title: "Shared in our social networks",
+      subtitle: "Linkedin and twitter.",
+    },
+  ];
+  return (
+    <div class="card card-bordered border-primary shadow bg-base-200">
+      <div class="card-body">
+        <div class="card-title">
+          <div class="form-control w-full">
+            <label class="cursor-pointer label items-start">
+              <span class="label-text font-bold text-lg">Base posting</span>
+              <div>
+                <input
+                  type="checkbox"
+                  class="toggle toggle-primary"
+                  checked
+                  disabled
+                />
+                <div class="font-bold text-sm">$10.00</div>
+              </div>
+            </label>
+          </div>
+        </div>
+
+        <ul class="flex flex-col gap-y-4">
+          {postingConditions.map(({ subtitle, title }, i) => (
+            <li key={i}>
+              <div class="flex gap-3">
+                <div class="flex-none">
+                  <iconify-icon
+                    width={18}
+                    height={18}
+                    icon="icon-park-outline:check-correct"
+                    class="text-success"
+                  ></iconify-icon>
+                </div>
+
+                <div>
+                  <div class="font-semibold leading-none">{title}</div>
+                  <div class="text-sm mt-1">{subtitle}</div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+});
--- src/routes/post-a-job/place-order.tsx
+++ src/routes/post-a-job/place-order.tsx
@@ -1,30 +1,19 @@
-import { type QRL, component$ } from "@builder.io/qwik";
+import { type QRL, component$, useSignal } from "@builder.io/qwik";
 import { PageHeader } from "./page-header";
 import { PriceConditionCard } from "./price-condition-card";
 import { Button } from "~/components/ui/actions/button";
 import { Logo } from "~/components/logo";
+import { BasePriceCard } from "./base-price-card";
 
 interface PlaceOrderProps {
   onTabChange: QRL<(tab: number) => {}>;
 }
 export const PlaceOrder = component$((props: PlaceOrderProps) => {
   const { onTabChange } = props;
-  const postingConditions = [
-    {
-      title: "Active for 30 days",
-      subtitle: "Your opening will be live for 30 days.",
-    },
-    {
-      title: "Included in our weekly newsletter",
-      subtitle:
-        "Get your job delivered to our entire database of active job seekers.",
-    },
+  const price = useSignal(10);
+  const bringToTop = useSignal(false);
+  const isFeatured = useSignal(false);
 
-    {
-      title: "Shared in our social networks",
-      subtitle: "Linkedin and twitter.",
-    },
-  ];
   return (
     <div class="grid grid-cols-12 h-full gap-6 divide-x absolute inset-0">
       <div class="col-span-12 lg:col-span-8 py-4 max-w-3xl mx-auto">
@@ -36,50 +25,7 @@ export const PlaceOrder = component$((props: PlaceOrderProps) => {
           subtitle="Get more visibility with add-ons. Configure your opening with these optional add-ons:"
         />
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
-          <div class="card card-bordered shadow bg-base-200">
-            <div class="card-body">
-              <div class="card-title">
-                <div class="form-control w-full">
-                  <label class="cursor-pointer label items-start">
-                    <span class="label-text font-bold text-lg">
-                      Base posting
-                    </span>
-                    <div>
-                      <input
-                        type="checkbox"
-                        class="toggle toggle-primary"
-                        checked
-                      />
-                      <div class="font-bold text-sm">$10.00</div>
-                    </div>
-                  </label>
-                </div>
-              </div>
-
-              <ul class="flex flex-col gap-y-4">
-                {postingConditions.map(({ subtitle, title }, i) => (
-                  <li key={i}>
-                    <div class="flex gap-3">
-                      <div class="flex-none">
-                        <iconify-icon
-                          width={18}
-                          height={18}
-                          icon="icon-park-outline:check-correct"
-                          class="text-success"
-                        ></iconify-icon>
-                      </div>
-
-                      <div>
-                        <div class="font-semibold leading-none">{title}</div>
-                        <div class="text-sm mt-1">{subtitle}</div>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-
+          <BasePriceCard />
           <div>
             <ul class="flex flex-col gap-y-4">
               <PriceConditionCard
@@ -144,7 +90,7 @@ export const PlaceOrder = component$((props: PlaceOrderProps) => {
               fullWidth
               colorScheme="btn-primary"
             >
-              Post my job for $20
+              Post my job for ${price.value}
             </Button>
 
             <p class="text-xs prose mt-4">
