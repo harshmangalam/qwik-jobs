109d16e1e72291979b3cf3f9b370db7b185849c2
harshmangalam <harshdev8218@gmail.com>
2023-07-21 12:18:54 +0530

    added blob destroy

--- src/utils/blob.ts
+++ src/utils/blob.ts
@@ -1,18 +1,28 @@
+import crypto from "crypto";
+
+const cloudName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
+const uploadPreset = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
+const apiKey = import.meta.env.VITE_CLOUDINARY_API_KEY;
+const apiSecret = import.meta.env.VITE_CLOUDINARY_API_SECRET;
+const generateSHA1 = (data: any) => {
+  const hash = crypto.createHash("sha1");
+  hash.update(data);
+  return hash.digest("hex");
+};
+
+const generateSignature = (publicId: string, apiSecret: string) => {
+  const timestamp = new Date().getTime();
+  return `public_id=${publicId}&timestamp=${timestamp}${apiSecret}`;
+};
+
 export const fileUpload = async (file: File) => {
-  console.log(import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
-  console.log(import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET);
   const formdata = new FormData();
   formdata.append("file", file);
-  formdata.append("cloud_name", import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
-  formdata.append(
-    "upload_preset",
-    import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
-  );
+  formdata.append("cloud_name", cloudName);
+  formdata.append("upload_preset", uploadPreset);
 
   const endpoint =
-    "https://api.cloudinary.com/v1_1/" +
-    import.meta.env.VITE_CLOUDINARY_CLOUD_NAME +
-    "/auto/upload";
+    "https://api.cloudinary.com/v1_1/" + cloudName + "/auto/upload";
 
   const res = await fetch(endpoint, {
     body: formdata,
@@ -22,4 +32,20 @@ export const fileUpload = async (file: File) => {
   const data = await res.json();
 
   return data;
+};
+
+export const destroyBlob = async (publicId: string) => {
+  const endpoint = `https://api.cloudinary.com/v1_1/${cloudName}/image/destroy`;
+  const signature = generateSHA1(generateSignature(publicId, apiSecret));
+  const timestamp = new Date().getTime();
+  const res = await fetch(endpoint, {
+    body: JSON.stringify({
+      public_id: publicId,
+      signature: signature,
+      api_key: apiKey,
+      timestamp: timestamp,
+    }),
+    method: "post",
+  });
+  return res.json();
 };
